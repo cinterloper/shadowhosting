8e40db51fb0fb92c4e5fa61d37369274f1078e53
root <grant@iowntheinter.net>
2015-10-28 06:20:25 +0000

    updates

--- docker/.util/build.sh
+++ docker/.util/build.sh
@@ -4,5 +4,5 @@ COMPONENT=$1
 
 cp $HOME/.ssh/id_rsa $COMPONENT/id.rsa
 cd $COMPONENT
-docker build -t $BASEREPO/$(cat meta.json | jq -r .tag) --no-cache .
+docker build -t $BASEREPO/$(cat meta.json | jq -r .tag) .
 bash init.sh
