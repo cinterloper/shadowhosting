00ed261712b66fbcfbd353a6c027678c759229fe
grant haywood <grant@iowntheinter.net>
2015-10-31 06:18:43 +0000

    pull latest if not one of ours

--- controllerContainer/.util/build.sh
+++ controllerContainer/.util/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 BASEREPO='shadowhosting'
 COMPONENT=$1
-
+BASEI=$(grep FROM $1/Dockerfile | cut -d ' ' -f 2-)
+if [[ $(echo $BASEI | grep -c $BASEREPO) -lt 1 ]]; then
+ docker pull $BASEI
+fi
 cp $HOME/.ssh/id_rsa $COMPONENT/id.rsa
 cd $COMPONENT
 docker build -t $BASEREPO/$(cat meta.json | jq -r .tag) .
-bash init.sh
+if [[ -a init.sh ]]; then
+  bash init.sh
+fi
