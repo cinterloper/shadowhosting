02dd6f90a7d98f4e07f3a0f3bd33b7a3dca9f6be
grant haywood <grant@iowntheinter.net>
2015-11-04 06:50:32 +0000

    messy

--- controllerApp/src/main/groovy/net/iowntheinter/shadow/controller/impl/shellOperatorConsole.groovy
+++ controllerApp/src/main/groovy/net/iowntheinter/shadow/controller/impl/shellOperatorConsole.groovy
@@ -3,20 +3,33 @@ package net.iowntheinter.shadow.controller.impl
 import io.vertx.groovy.core.Vertx
 import io.vertx.groovy.ext.shell.command.CommandBuilder
 import io.vertx.groovy.ext.shell.ShellService
+
 /**
  * Created by grant on 10/28/15.
  */
 class shellOperatorConsole {
     def cmdset
-    shellOperatorConsole(Vertx v){
+    ShellService svc
+
+    shellOperatorConsole(Vertx v) {
+        svc = ShellService.create(vertx, [
+                telnetOptions: [
+                        host: "localhost",
+                        port: 3000
+                ]
+        ])
+        def shdwcmd = CommandBuilder.command("ShadowCmd").processHandler(OCProcHdlr).build()
+        svc.getCommandRegistry().registerCommand(shdwcmd)
+
 
     }
+    void SetupCredsHdlr(process){
 
-    void OCProcHdlr(process){
+    }
+    void OCProcHdlr(process) {
         process.write("hello world\n")
         process.end()
     }
 
-    def shdwcmd = CommandBuilder.command("ShadowCmd").processHandler(OCProcHdlr).build()
 }
 
---
+++ controllerApp/src/main/groovy/net/iowntheinter/shadow/controller/shadowConfiguration.groovy
@@ -0,0 +1,7 @@
+package net.iowntheinter.shadow.controller
+
+/**
+ * Created by grant on 10/31/15.
+ */
+interface shadowConfiguration {
+}
