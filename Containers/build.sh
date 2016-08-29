2484bd1e64d0e075405df1bee6fe88235dbcec3d
Grant Haywood <grant@iowntheinter.net>
2016-08-28 20:36:22 -0700

    back off salt 2016 to 2015.8.8(ubunu pkg) utill salt issue #34506 is fixed

--- Containers/build.sh
+++ Containers/build.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
+VENDOR=shadowsystem
 fail() {
-    echo "ERROR IN BUILD"
+    echo "ERROR IN BUILD $@"
     kill -9 $$
 }
 
+log() {
+  echo $@
+}
+
+
 {
-  docker build -t shadowsystem/baseimage .
+  docker build $BUILDARGS -t $VENDOR/baseimage .
 } || {
   fail
 }
-for ctrdir in $(find . -maxdepth 1 -mindepth 1 -type d | grep -v _)
+
+
+
+for ctrdir in $(find Containers/ -maxdepth 1 -mindepth 1 -type d | grep -v _)
 do
   cd $(realpath $ctrdir);
-  {
-    docker build -t shadowsystem/${PWD##*/} .
-  } || {
-    fail
-  }
-  cd ..;
+  log "building $VENDOR/${PWD##*/} "; echo
+  if [[ ! -f _BUILD_DISABLE ]]; then
+    if [[ -f add.sh ]]; then
+      {
+        bash add.sh add $EXTRA
+       } || {
+        fail
+      }
+    fi
+    {
+      docker build -t $VENDOR/${PWD##*/} .
+    } || {
+      fail
+    }
+  fi
+  cd ../../;
 done
-
-
-
---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM cinterloper/cornerstone-base
+RUN apt-get update; apt -y upgrade; apt-get -y install wget unzip python python-pip salt-master salt-minion
+RUN echo baseimage > /etc/salt/minion_id
+ADD Containers/_salt /srv/salt
+RUN salt-call --local state.highstate #pillar='{"sys_packages":'$(cat /srv/salt/pkgs.json)'}'
+ENV CORNERSTONE_HOST='172.17.0.1'
+ENV KVDN_START_TIME="5"
+ENV MY_START_TIME="1"
+ENV FAILURE_HOOK="echo FAILURE"
+ENV STARTUP_HOOKS="/startup.sh"
+CMD /init.sh
